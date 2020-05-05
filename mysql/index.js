1f617aa117c1ed514f3a72d35bd1aac8b1dba643
xiaqingpeng <626143872@qq.com>
2020-05-05 18:26:44 +0800

    数据库数据输出到前端

--- mysql/index.js
+++ mysql/index.js
@@ -1,5 +1,9 @@
 // get the client
 const mysql = require("mysql");
+const  Koa = require("koa");
+// 创建一个Koa对象表示web app本身:
+const app = new Koa();
+
 (async () => {
   // create the connection to database
   const connection = mysql.createConnection({
@@ -12,18 +16,40 @@ const mysql = require("mysql");
   connection.connect((err) => {
     console.log(err);
   });
-  var sql = `SELECT * FROM person`;
+  var sql1 = `SELECT * FROM person`;
+  var sql2 = `SELECT * FROM frame`;
+  let str1,str2
+
+
   //查
-  connection.query(sql, function (err, result) {
+  connection.query(sql1, function (err, result) {
     if (err) {
       console.log("[SELECT ERROR] - ", err.message);
       return;
     }
 
+    str1=result
+    console.log(result);
   
+  });
+  //查
+  connection.query(sql2, function (err, result) {
+    if (err) {
+      console.log("[SELECT ERROR] - ", err.message);
+      return;
+    }
+
+   str2=result
     console.log(result);
   
   });
 
+  app.use(async (ctx, next) => {
+    await next();
+    ctx.response.type = 'text/html';
+    ctx.body = [...str1,...str2];
+ });
+ // 在端口3000监听:
+app.listen(3000);
   connection.end();
 })();
